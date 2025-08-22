204ec9eb28ad227c9722bbaa90061f1756292061
selenatnguyen <85751374+selenatnguyen@users.noreply.github.com>
2025-08-22 09:36:50 -0700

    Update part1_etl.py

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -11,8 +11,22 @@ import os
 import pandas as pd
 import json
 
-# Create '/data' directory if it doesn't exist
 data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
 os.makedirs(data_dir, exist_ok=True)
 
-# Load datasets and save to '/data'
+def etl():
+    # Create '/data' directory if it doesn't exist
+    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+    os.makedirs(data_dir, exist_ok=True)
+
+    rows = []
+    with open("imdb_movies_2000to2022.prolific.json", "r", encoding="utf-8") as f:
+        for line in f:
+            rows.append(json.loads(line))
+    df = pd.DataFrame(rows)
+    df.reset_index(inplace=True)
+    df.rename(columns={'index':'id'}, inplace=True)
+
+    out_path = os.path.join(data_dir, "imdb_movies.csv")
+    df.to_csv(out_path, index=False)
+    return df
